09232dd168633b7e2f1df88e1fc28e833065a114
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-25 14:13:26 +0100

    login fix

--- assets/php/database/db_connect.php
+++ assets/php/database/db_connect.php
@@ -5,7 +5,7 @@ function OpenCon()
     $dbhost = "ec2-63-32-248-14.eu-west-1.compute.amazonaws.com";
     $dbuser = "zktcmdwimhgred";
     $dbpass = "e4db533510772eaa0fc6096147667a412250f43cd57f2dc60cb5cabb6a6c7ffb";
-    $db = "ei_web_project";
+    $db = "d7l5frqabneo71";
     $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
     return $conn;
