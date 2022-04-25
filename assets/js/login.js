009c6abbee92fee24f081ff16cd214eb654459d7
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-25 14:17:35 +0100

    login fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         $.ajax({
             type: "POST",
             enctype: 'multipart/form-data',
-            url: "php/authentication/login.php",
+            url: "../php/authentication/login.php",
             data: data,
             processData: false,
             contentType: false,
