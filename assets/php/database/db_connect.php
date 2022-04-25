892c27115328c897256e7d0c6c2be3754ea428c6
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-25 19:39:23 +0100

    login fix

--- assets/php/database/db_connect.php
+++ assets/php/database/db_connect.php
@@ -7,7 +7,13 @@ function OpenCon()
     $dbpass = "e4db533510772eaa0fc6096147667a412250f43cd57f2dc60cb5cabb6a6c7ffb";
     $db = "d7l5frqabneo71";
     //$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
-    $conn = pg_connect($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+    $conn = pg_connect("host=$dbhost dbname=$db user=$dbuser password=$dbpass"); 
+    echo "connected to database"
+    if(!$conn){
+        die("Connection failed: %s\n". $conn -> error);
+    }
+    
+    //or die("Connect failed: %s\n". $conn -> error);
     return $conn;
 }
 
