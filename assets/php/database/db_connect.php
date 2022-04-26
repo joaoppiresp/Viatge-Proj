cf5334013a8f7b4dcf4a136c662784b4dba2fa2c
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-26 19:56:35 +0100

    login fix

--- assets/php/database/db_connect.php
+++ assets/php/database/db_connect.php
@@ -4,7 +4,7 @@ function OpenCon()
 {
     $conn_string ="host=ec2-63-32-248-14.eu-west-1.compute.amazonaws.com port=5432 dbname=d7l5frqabneo71 user=zktcmdwimhgred password=e4db533510772eaa0fc6096147667a412250f43cd57f2dc60cb5cabb6a6c7ffb";
 
-    $dbconn = pg_connect($conn_string) || die("Connection not successfull");
+    $dbconn = pg_connect($conn_string) or die("Connection not successfull");
     
     //return $dbconn;
 
@@ -12,17 +12,20 @@ function OpenCon()
     if($dbconn){
         echo'Connected to database<br></br>';
         $result = pg_query($dbconn, "SELECT * FROM users");
-        while($row = pg_fetch_row($result)){ 
-            echo $row[0],"<br></br>";
-            echo $row[1],"<br></br>";
-            echo $row[2],"<br></br>";
-            echo $row[3],"<br></br>";
-            echo $row[4],"<br></br>";
-
+        if(!$result){
+            echo'Failed to query database<br></br>';
+        }else{
+            while($row = pg_fetch_row($result)){ 
+                echo $row[0],"<br></br>";
+                echo $row[1],"<br></br>";
+                echo $row[2],"<br></br>";
+                echo $row[3],"<br></br>";
+                echo $row[4],"<br></br>";
+    
+            }
         }
-
     }else{
-        echo'connection failed';
+        echo'Connection failed';
         exit;
     }
 }
