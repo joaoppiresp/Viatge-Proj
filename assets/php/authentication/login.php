585941f7b989280c76ce75c5c679d0a5075b36c0
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 08:42:28 +0100

    login fix

--- assets/php/authentication/login.php
+++ assets/php/authentication/login.php
@@ -5,23 +5,24 @@ include '../database/init_connection.php';
 //$email = $_POST['uemail'];
 //$password = $_POST['psw'];
 //$password = md5($_POST['passwrd']);
+$email = 'testusers@gmail.com';
+$password = 'password123';
 
 if(!empty($email) && !empty($password))
 {
-    $sql = "SELECT * FROM users WHERE email=testuser@gmail.com AND passwrd=password123;";
-    //$sql = "SELECT * FROM users WHERE email=$email AND passwrd=$password;";
+    $sql = "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'";
     
     $result = pg_query($dbconn, $sql);
+    $result = pg_fetch_array($result);
 
     if($result)
     {
-        while($arr = pg_fetch_array($result)){
-            //$result_array[] = $row;
-            echo $arr[0]."<br></br>";
-            echo $arr[1]."<br></br>";
-            echo $arr[2]."<br></br>";
-            echo $arr[3]."<br></br>";
-            echo $arr[4]."<br></br>";
+        while($result = pg_fetch_array($result)){
+            echo $result[0]."<br></br>";
+            echo $result[1]."<br></br>";
+            echo $result[2]."<br></br>";
+            echo $result[3]."<br></br>";
+            echo $result[4]."<br></br>";
         }
         //echo json_encode($result_array);
     }
