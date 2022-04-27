ebc9254725a0da47f42afcfc8113795a6adfbcdb
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 23:50:18 +0100

    login fix

--- assets/php/profile.php
+++ assets/php/profile.php
@@ -8,13 +8,7 @@ $email = 'testuser@gmail.com';
 $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email . "'");
 
 while($row = pg_fetch_array($result)){ 
-     if(json_encode($row)==='["f"]'){
-        $value = '0'; //querie didn't work
-        echo json_encode($value);
-    }else{
-        echo json_encode($row);
-    }
-
+     echo json_encode($row);
 }
 
 CloseCon($dbconn);
