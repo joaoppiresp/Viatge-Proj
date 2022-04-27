735db69e1f6c7de62cbd3b0ea3be7e3c5f687107
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 08:58:43 +0100

    login fix

--- assets/php/authentication/login.php
+++ assets/php/authentication/login.php
@@ -10,22 +10,23 @@ $password = 'password123';
 
 if(!empty($email) && !empty($password))
 {
-    $sql = "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'";
+    //$sql = "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'";
     
-    $result = pg_query($dbconn, $sql);
+    $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'");
     $result = pg_fetch_array($result);
 
     if($result)
     {
-        while($result = pg_fetch_array($result)){
-            echo $result[0];
-        }
+        while($row = pg_fetch_row($result)){ 
+            echo $row[0],"<br></br>";
+            echo $row[1],"<br></br>";
+            echo $row[2],"<br></br>";
+            echo $row[3],"<br></br>";
+            echo $row[4],"<br></br>";
         //echo json_encode($result_array);
+        }
     }
-    else
-    {
-        echo'failed to follow through';
-    }
+
 }else{
     echo'param email and password not empty';
 }
