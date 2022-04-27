ae3d5ed5210ca385d192dc2372272207e30dba3a
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-28 00:04:50 +0100

    login fix

--- assets/php/profile.php
+++ assets/php/profile.php
@@ -10,19 +10,9 @@ $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email .
 if(!$result){
     echo 'query failed';
 }else{
-    while($row = pg_fetch_row($result)){ 
-        echo json_encode($row[0]);
-        echo '<br></br>'
-        echo json_encode($row[1]);
-        echo '<br></br>'
-        echo json_encode($row[2]);
-        echo '<br></br>'
-        echo json_encode($row[3]);
-        echo '<br></br>'
-        echo json_encode($row[4]);
-        echo '<br></br>'
-        echo json_encode($row[5]);
-        echo '<br></br>'
+    while($row = pg_fetch_array($result)){ 
+        echo json_encode($row);
+       
    }
 }
 
