b300246ac9dd93f77a4deb0032f41507c383e29c
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 13:10:40 +0100

    login fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
         //stop submit the form, we will post it manually.
         event.preventDefault();
         // Get form
-        //var form = $('#loginForm')[0];
+        var form = $('#loginForm')[0];
         // FormData object 
-        //var data = new FormData(form);
+        var data = new FormData(form);
         // disabled the submit button
         $("#btnSubmit").prop("disabled", true);
  
@@ -13,19 +13,19 @@ $(document).ready(function () {
             type: "POST",
             enctype: 'multipart/form-data',
             url: "assets/php/authentication/login.php",
-            data: { uemail: $('#uemail').val() , psw: $('#psw').val()},
+            data: data,
             processData: false,
             contentType: false,
             cache: false,
             timeout: 800000,
-            success: function (){
+            success: function (data){
 
                 if(data === '0'){
                     console.log("user not found, try again");
                     window.location.reload();
                 }else{
                     $("#btnSubmit").prop("disabled", false);
-                    console.log("SUCCESS : ", $user_info);
+                    console.log("SUCCESS : ", data);
                     //window.location.replace("index.php");
                 }
 
