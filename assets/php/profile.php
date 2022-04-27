9d3a7001d469ca1f6fe1f1d3d4593f9bd14e88e8
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 23:51:15 +0100

    login fix

--- assets/php/profile.php
+++ assets/php/profile.php
@@ -6,10 +6,11 @@ include '../database/init_connection.php';
 $email = 'testuser@gmail.com';
 
 $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email . "'");
-
+/*
 while($row = pg_fetch_array($result)){ 
      echo json_encode($row);
 }
+*/
 
 CloseCon($dbconn);
 
