30ce2efdb5be1a17159a3e04f0ec72ae6ab2a535
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 09:10:43 +0100

    login fix

--- assets/php/database/db_connect.php
+++ assets/php/database/db_connect.php
@@ -37,7 +37,7 @@ function OpenCon()
     
         //$result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'");
         $result = pg_query($dbconn,"SELECT * FROM users");
-        $result = pg_fetch_array($result);
+        //$result = pg_fetch_array($result);
 
         if($result)
         {
