b04fe04c9b68ea2a3a2dbb907ca8ee5bbee7ef37
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-28 00:09:30 +0100

    login fix

--- assets/php/profile.php
+++ assets/php/profile.php
@@ -10,7 +10,7 @@ $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email .
 if(!$result){
     echo 'query failed';
 }else{
-    while($row = pg_fetch_array($result)){ 
+    while($row = pg_fetch_row($result)){ 
         echo json_encode($row[0]);
         echo '<br></br>';
         echo json_encode($row[1]);
