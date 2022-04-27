020d9cbc1afcd84cc9401a9a0a87bcd44147147d
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 16:54:21 +0100

    login fix

--- login-page.php
+++ login-page.php
@@ -60,7 +60,7 @@
                                 <div class="panel-body tabs-menu-body p-0 pt-5">
                                     <div class="tab-content">
                                         <div class="tab-pane active" id="tab5">
-                                            <form id="loginForm" method="POST">
+                                            <form id="loginForm">
                                                 <label for="uemail"><b>Email</b></label>
                                                 <input class="input100 border-start-0 form-control ms-0" type="text" placeholder="Email" name="uemail" required>
 
@@ -71,11 +71,6 @@
                                                 <div class="text-end pt-4">
                                                     <p class="mb-0"><a href="forgot-password.html" class="text-primary ms-1">Forgot Password?</a></p>
                                                 </div>
-                                                <!--
-                                                <div class="container-login100-form-btn">
-                                                    <input class="login100-form-btn btn-primary" type="submit" value="Submit">
-                                                </div>
-                                                -->
                                             </form>        
                                             <div class="text-center pt-3">
                                                 <p class="text-dark mb-0">Not a member?<a href="register.html" class="text-primary ms-1">Sign UP</a></p>
