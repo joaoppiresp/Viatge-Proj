15b4a742c11e902a88b7e63207e3747f41824ac8
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 11:29:25 +0100

    login fix

--- assets/php/authentication/login.php
+++ assets/php/authentication/login.php
@@ -6,15 +6,16 @@ include '../database/init_connection.php';
 //$password = $_POST['psw'];
 //$password = md5($_POST['passwrd']);
 $email = 'testuser@gmail.com';
-$password = 'password123';
+$password = 'password1234';
 
 if(!empty($email) && !empty($password))
 {
 
     $result = pg_query($dbconn, "SELECT * FROM users WHERE users.email='" . $email . "'AND users.passwrd='" . $password . "'");
 
-    if(!$result){
-        echo pg_last_error();
+    if($result === FALSE){
+        $error = pg_last_error();
+        echo $error;
        
     }else{
         while($row = pg_fetch_row($result)){ 
