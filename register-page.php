83d90494634f7ee6c9cc85beb94aeca803295238
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 20:03:07 +0100

    login fix

--- register-page.php
+++ register-page.php
@@ -49,28 +49,18 @@
                 </div>
                 <div class="container-login100">
                     <div class="wrap-login100 p-6">
-                        <form class="login100-form validate-form">
+                        <form class="register-ajax" action="assets/php/authentication/login.php" method="POST">
                             <span class="login100-form-title">
-									Registration
-								</span>
-                            <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
-                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                    <i class="mdi mdi-account" aria-hidden="true"></i>
-                                </a>
-                                <input class="input100 border-start-0 ms-0 form-control" type="text" placeholder="User name">
-                            </div>
-                            <div class="wrap-input100 validate-input input-group" data-bs-validate="Valid email is required: ex@abc.xyz">
-                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                    <i class="zmdi zmdi-email" aria-hidden="true"></i>
-                                </a>
-                                <input class="input100 border-start-0 ms-0 form-control" type="email" placeholder="Email">
-                            </div>
-                            <div class="wrap-input100 validate-input input-group" id="Password-toggle">
-                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
-                                    <i class="zmdi zmdi-eye" aria-hidden="true"></i>
-                                </a>
-                                <input class="input100 border-start-0 ms-0 form-control" type="password" placeholder="Password">
-                            </div>
+								Registration
+							</span>
+                            <label for="uename"><b>Name</b></label>
+                            <input type="text" placeholder="User name" name="uename">
+                            <label for="uemail"><b>Email</b></label>
+                            <input type="text" placeholder="Email" name="uemail">
+                            <label for="psw"><b>Password</b></label>
+                            <input class="input100 border-start-0 form-control ms-0" type="password" placeholder="Password" name="psw">
+                            <input class="login100-form-btn btn-primary" type="submit" value="Submit" id="btnSubmit">
+
                             <label class="custom-control custom-checkbox mt-4">
 									<input type="checkbox" class="custom-control-input">
 									<span class="custom-control-label">Agree the <a href="terms.html">terms and policy</a></span>
