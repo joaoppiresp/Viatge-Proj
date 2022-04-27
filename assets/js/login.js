cedd9f9fbde2e45449183c9a327a579c46d0cea6
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 16:59:51 +0100

    login fix

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,15 +2,15 @@ $(document).ready(function () {
     $("#loginForm").submit(function (event) {
 
         event.preventDefault();
-        // FormData object 
-        //var data = new FormData($('#loginForm')[0]);
+        //FormData object 
+        var data = new FormData($('#loginForm')[0]);
         $("#btnSubmit").prop("disabled", true);
  
         $.ajax({
             type: "POST",
-            //enctype: 'multipart/form-data',
+            enctype: 'multipart/form-data',
             url: "assets/php/authentication/login.php",
-            data: $("#loginForm").serialize(),
+            data: data,
             processData: false,
             contentType: false,
             cache: false,
