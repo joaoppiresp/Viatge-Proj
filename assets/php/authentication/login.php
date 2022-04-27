b42d8f50a69f862e8326db9b061526a6a82d1d38
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-04-27 11:09:35 +0100

    login fix

--- assets/php/authentication/login.php
+++ assets/php/authentication/login.php
@@ -6,7 +6,7 @@ include '../database/init_connection.php';
 //$password = $_POST['psw'];
 //$password = md5($_POST['passwrd']);
 $email = 'testuser@gmail.com';
-$password = 'password123';
+$password = 'password12';
 
 if(!empty($email) && !empty($password))
 {
