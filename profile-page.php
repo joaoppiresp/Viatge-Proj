c92afe9cba9e5425c9e910521fa5638401a7c6a4
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-05-04 12:08:26 +0100

    after reset commit

--- profile-page.php
+++ profile-page.php
@@ -266,6 +266,58 @@
                                         </div>
                                     </div>
                                 </div>
+                                <div class="card">
+                                    <div class="card-header">
+                                        <div class="card-title">Edit Password</div>
+                                    </div>
+                                    <div class="card-body">
+                                        <div class="text-center chat-image mb-5">
+                                            <div class="avatar avatar-xxl chat-profile mb-3 brround">
+                                                <a class="" href="profile.html"><img alt="avatar" src="../assets/images/users/7.jpg" class="brround"></a>
+                                            </div>
+                                            <div class="main-chat-msg-name">
+                                                <a href="profile.html">
+                                                    <h5 class="mb-1 text-dark fw-semibold">Percy Kewshun</h5>
+                                                </a>
+                                                <p class="text-muted mt-0 mb-0 pt-0 fs-13">Web Designer</p>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label">Current Password</label>
+                                            <div class="wrap-input100 validate-input input-group" id="Password-toggle">
+                                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                    <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                                </a>
+                                                <input class="input100 form-control" type="password" placeholder="Current Password">
+                                            </div>
+                                            <!-- <input type="password" class="form-control" value="password"> -->
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label">New Password</label>
+                                            <div class="wrap-input100 validate-input input-group" id="Password-toggle1">
+                                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                    <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                                </a>
+                                                <input class="input100 form-control" type="password" placeholder="New Password">
+                                            </div>
+                                            <!-- <input type="password" class="form-control" value="password"> -->
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="form-label">Confirm Password</label>
+                                            <div class="wrap-input100 validate-input input-group" id="Password-toggle2">
+                                                <a href="javascript:void(0)" class="input-group-text bg-white text-muted">
+                                                    <i class="zmdi zmdi-eye text-muted" aria-hidden="true"></i>
+                                                </a>
+                                                <input class="input100 form-control" type="password" placeholder="Confirm Password">
+                                            </div>
+                                            <!-- <input type="password" class="form-control" value="password"> -->
+                                        </div>
+                                    </div>
+                                    <div class="card-footer text-end">
+                                        <a href="javascript:void(0)" class="btn btn-primary">Update</a>
+                                        <a href="javascript:void(0)" class="btn btn-danger">Cancel</a>
+                                    </div>
+                                </div>
                                 <div class="row">
                                     <div class="col-xl-3">
                                         <div class="card">
