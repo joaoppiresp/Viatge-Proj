a33b91de3482394d484f88d2ed58b64e1672ef4b
joaoppiresp <75837129+joaoppiresp@users.noreply.github.com>
2022-05-05 15:13:40 +0100

    color updates

--- profile-page.php
+++ profile-page.php
@@ -178,19 +178,6 @@
 
                     <!-- CONTAINER -->
                     <div class="main-container container-fluid">
-
-                        <!-- PAGE-HEADER -->
-                        <div class="page-header">
-                            <h1 class="page-title">Profile</h1>
-                            <div>
-                                <ol class="breadcrumb">
-                                    <li class="breadcrumb-item"><a href="javascript:void(0)">Pages</a></li>
-                                    <li class="breadcrumb-item active" aria-current="page">Profile</li>
-                                </ol>
-                            </div>
-                        </div>
-                        <!-- PAGE-HEADER END -->
-
                         <!-- ROW-1 OPEN -->
                         <div class="row" id="user-profile">
                             <div class="col-lg-12">
